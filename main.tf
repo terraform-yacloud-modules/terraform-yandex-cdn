6beb0008392ef53d91945f1af3ff9a1ac133150c
Anton Patsev <patsev.anton@gmail.com>
2024-07-28 17:00:23 +0600

    Исправил некоторые ошибки tflint

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "yandex_cdn_origin_group" "main" {
-  name     = replace(var.cname, ".", "-")
-  use_next = var.origin_group_use_next
+  name      = replace(var.cname, ".", "-")
+  use_next  = var.origin_group_use_next
   folder_id = local.folder_id
 
   dynamic "origin" {
